ffa9e8e37cb0f6e51c5221851817d9211a572ce7
Danny <danny@dannychambers.co.uk>
2014-03-23 15:00:47 +0000

    Added theremin.js to allow access to and control of a sinewave. Added a access to the webcam and code to return data about the rgba values of the streaming input.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,9 +7,15 @@ $(document).ready(function () {
 	IMBUE.init = (function() {
 		console.log('Called init');
 
-		var pointer, pointerX = 0, pointerY = 0,
+		var pointer, pointerX = 0, 
+			pointerY = 0,
 			playing = false,
-			audioContext, jsNode, theremin;
+			audioContext,
+			jsNode,
+			theremin,
+			video = $("#live").get()[0],
+			canvas = $("#canvas"),
+			ctx = canvas.get()[0].getContext('2d');
 
 		/*var frequencyToNoteNumber = function(f) {
 			console.log('Called frequencyToNoteNumber');
@@ -34,7 +40,101 @@ $(document).ready(function () {
 			return noteNumberToNote(frequencyToNoteNumber(f));
 		}*/
 
-		function audioProcess(event) {
+		window.addEventListener('DOMContentLoaded', function() {
+			var isStreaming = false,
+			    video = document.getElementById('video'),
+			    canvas = document.getElementById('canvas'),
+			    con = canvas.getContext('2d'),
+			    w = 200, 
+			    h = 200;
+
+			navigator.getUserMedia = (navigator.getUserMedia || 
+			                          navigator.webkitGetUserMedia || 
+			                          navigator.mozGetUserMedia || 
+			                          navigator.msGetUserMedia);
+
+			if (navigator.getUserMedia) {
+			   
+			    navigator.getUserMedia(
+			     {
+			        video:true,
+			        audio:false
+			     },        
+		         function(stream) {
+		            var url = window.URL || window.webkitURL;
+		            video.src = url ? url.createObjectURL(stream) : stream;
+		            video.play();
+		         },
+		         function(error) {
+		            console.log('Something went wrong. (error code ' + error.code + ')');
+		            return;
+		         }
+			  );
+			}
+			else {
+			  console.log('Sorry, the browser you are using doesn\'t support getUserMedia');
+			  return;
+			}
+
+			video.addEventListener('canplay', function(e) {
+			   if (!isStreaming) {
+			      // videoWidth isn't always set correctly in all browsers
+			      if (video.videoWidth > 0) h = video.videoHeight / (video.videoWidth / w);
+			      canvas.setAttribute('width', w);
+			      canvas.setAttribute('height', h);
+			      // Reverse the canvas image
+			      con.translate(w, 0);
+			      con.scale(-1, 1);
+			      isStreaming = true;
+			   }
+			}, false);
+
+
+			video.addEventListener('play', function() {
+	
+				// Every 3 second copy the video image to the canvas
+				setInterval(function() {
+
+					var red = [],
+					    green = [],
+					    blue = [],
+					    alpha = [];
+
+					if (video.paused || video.ended) return;
+					con.fillRect(0, 0, w, h);
+					con.drawImage(video, 0, 0, w, h);
+
+					var imgData = con.getImageData(0,0,canvas.width,canvas.height);
+					var data = imgData.data;
+    				var index = 0;
+
+				    for (var y = 0; y < 100; y += 2) {
+
+				        for (var x = 0; x < 200; x += 2) {
+				            index = (y * imgData.width + x) * 4;
+
+				            red.push(data[index]);
+				            green.push(data[index + 1]);
+				            blue.push(data[index + 2]);
+				            alpha.push(data[index + 3]);
+
+
+				        }
+
+				    }
+
+				    // log out any pixel here
+				    console.log(red[1]);
+
+				}, 3000);
+
+
+			}, false);
+
+		});
+
+
+		var audioProcess = function(event) {
 			console.log('Called audioProcess');
 
 			var buffer = event.outputBuffer,
